69fa9c1cdc01bc233a6e326a8115124f9f46d175
Patrick Keating <keating916@gmail.com>
2021-09-27 21:54:07 -0700

    move contact mailer to api

--- pages/api/contactAPI.js
+++ pages/api/contactAPI.js
@@ -1,13 +1,13 @@
-import emailjs from 'emailjs-com';
+import axios from 'axios'
 
 export default async (req, res) => {
-    const user_ID = "user_mX30CEcEYTVb7EG39rmYu"        
+    /*const user_ID = "user_mX30CEcEYTVb7EG39rmYu"        
     const serviceID = 'RLS Contact Form';
     const templateID = 'RLS';
     const params = req.body.params
     emailjs.send(serviceID, templateID, params, user_ID )
     .then(response => {
-        console.log(response)
+        console.log(response.status, response.text)
         res.status(200).send(response)
     })
     .catch(error=> {
@@ -16,5 +16,55 @@ export default async (req, res) => {
     })
 
     res.status(200).send();
+    
+    let transporter = nodemailer.createTransport({
+        service: "Godaddy", // no need to set host or port etc.
+        auth: {
+            user: 'patrick@lodgepoleinc.com',
+            pass: 'Peter91625'
+        }
+   });
+   
+    let message = {
+        from: "patrick@lodgepoleinc.com",
+        to: "keatingdev25@gmail.com",
+        subject: "Message from contact page",
+        text: "HTML Message",
+        html: `<p>${req.body.params}</p>`
+    };
+
+    console.log(req.body.params)
+    res.status(200).send("OK")
+    transporter.sendMail(message)
+    .then(response => {
+        console.log(response)
+        res.send(response)
+    })
+    .catch(error => {
+        console.error(error);
+    }) */
+
+    const user_ID = "user_mX30CEcEYTVb7EG39rmYu"        
+    const serviceID = 'RLS Contact Form';
+    const templateID = 'RLS';
+    const template_params = req.body.params
+    const access_token = '216b45fad4abda169e2d724843d2ec58'
+    var data = {
+        service_id: serviceID,
+        template_id: templateID,
+        user_id: user_ID,
+        accessToken: access_token,
+        template_params: template_params
+    };
+
+    axios.post('https://api.emailjs.com/api/v1.0/email/send', data)
+    .then(response => {
+        console.log(response.data)
+        res.send(response.data)
+    })
+    .catch(error => {
+        console.log(error.response.data)
+    })
+
 
 }
--- pages/contact.js
+++ pages/contact.js
@@ -31,30 +31,47 @@ export default class Contact extends Component {
         let { fname, lname, contactEmail, contactPhone, address, comments } = this.state
         let params = {fname: fname, lname:lname, contactEmail:contactEmail, contactPhone:contactPhone, address:address, comments:comments}
 
-        let response = axios.post('http://localhost:3000/api/contactAPI', {params})
-        console.log(response)
+        axios.post('http://localhost:3000/api/contactAPI', {params})
+        .then(response => {
+            if(response.data === 'OK') {
+                this.setState({
+                    ...this.state,
+                    submitted: true,
+                })
+            }
+        }).catch(error => {
+            console.log(error);
+        })
     }
 
     render() {
         let { fname, lname, contactEmail, contactPhone, address } = this.state
 
         let d = (fname !== "" && lname !== "" && contactEmail !== "" && contactPhone !== "" && address !== "") ? false : true
-        return (
-            <main className={styles.main}>
-                <div className={styles.textCenter}>
-                    <h3>Quick Contact</h3>
-                        <form className={styles.formContainer} action='none'>
-                            <input className={styles.formControl} type="text" name="fname" placeholder="First Name" value={this.state.fname} onChange={this.handleChange} required />
-                            <input className={styles.formControl} type="text" name="lname" placeholder="Last Name"  value={this.state.lname} onChange={this.handleChange}  required />
-                            <input className={styles.formControl} type="email" name="contactEmail" placeholder="Email: you@yourdomain.com"  value={this.state.contactEmail} onChange={this.handleChange}  required />
-                            <input className={styles.formControl} type="phone" name="contactPhone" placeholder="Phone: 111-111-1111"  value={this.state.contactPhone}  required onChange={this.handleChange}  />
-                            <input className={styles.formControl} type="text" name="address" placeholder="Service Address"  value={this.state.address} onChange={this.handleChange}  required />
-                            <textarea name="comments" className={styles.formControl} placeholder="Comments" id="contactTextArea"  value={this.state.comments } onChange={this.handleChange}  ></textarea>
-                            <button className={styles.formControl} type="submit" id="formSubmitButton" disabled={d} onClick={evt => this.handleSubmit(evt)}>Submit</button>
-                        </form>
-                </div>
-            </main>
-            
-        )
+        if(!this.state.submitted) {
+            return (
+                <main className={styles.main}>
+                    <div className={styles.textCenter}>
+                        <h3>Quick Contact</h3>
+                            <form className={styles.formContainer} action='none'>
+                                <input className={styles.formControl} type="text" name="fname" placeholder="First Name" value={this.state.fname} onChange={this.handleChange} required />
+                                <input className={styles.formControl} type="text" name="lname" placeholder="Last Name"  value={this.state.lname} onChange={this.handleChange}  required />
+                                <input className={styles.formControl} type="email" name="contactEmail" placeholder="Email: you@yourdomain.com"  value={this.state.contactEmail} onChange={this.handleChange}  required />
+                                <input className={styles.formControl} type="phone" name="contactPhone" placeholder="Phone: 111-111-1111"  value={this.state.contactPhone}  required onChange={this.handleChange}  />
+                                <input className={styles.formControl} type="text" name="address" placeholder="Service Address"  value={this.state.address} onChange={this.handleChange}  required />
+                                <textarea name="comments" className={styles.formControl} placeholder="Comments" id="contactTextArea"  value={this.state.comments } onChange={this.handleChange}  ></textarea>
+                                <button className={styles.formControl} type="submit" id="formSubmitButton" disabled={d} onClick={evt => this.handleSubmit(evt)}>Submit</button>
+                            </form>
+                    </div>
+                </main>
+                
+            )
+        }else {
+            return(
+                <main className={styles.main}>
+                    <div className={styles.textCenter}>Thank you, your contact form has been submitted!</div>
+                </main>
+            )
+        }
     }
 }
